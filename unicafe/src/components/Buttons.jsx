3481271ddc15ab2c8c119718da74e5ca24b44577
Andres Laverde <andres.laverde@protonmail.com>
2026-08-27 16:56:46 -0700

    update: use store actions in Button

--- unicafe/src/components/Buttons.jsx
+++ unicafe/src/components/Buttons.jsx
@@ -1,10 +1,14 @@
+import { useStatsControls } from "../store";
+
 const Buttons = () => {
+  const { vote } = useStatsControls();
+
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={() => vote('good')}>good</button>
+      <button onClick={() => vote('neutral')}>neutral</button>
+      <button onClick={() => vote('bad')}>bad</button>
     </div>
   )
 }
