e6134fb057665dc8605fff1385e341e2a316aac3
Andres Laverde <andres.laverde@protonmail.com>
2026-08-27 16:56:10 -0700

    update: correct stats update in store actions

--- unicafe/src/store.js
+++ unicafe/src/store.js
@@ -2,21 +2,23 @@ import { create } from 'zustand';
 
 const useStatsStore = create(set => ({
   stats: {
-    good: 26,
-    neutral: 40,
-    bad: 0.3,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   },
   actions: {
     vote: (feedback) => set(state => {
-      if (feedback === 'good') {
-        return { good: state.stats.good + 1 };
+      
+      if (feedback === 'good') {     
+        return { stats: { ...state.stats, good: state.stats.good + 1 } };
       } else if (feedback === 'neutral') {
-        return { neutral: state.stats.neutral + 1 };
+        return { stats: { ...state.stats, neutral: state.stats.neutral + 1 } };
       } else if (feedback === 'bad') {
-        return { bad: state.stats.bad + 1 };
+        return { stats: { ...state.stats, bad: state.stats.bad + 1 } };
       }
     })
   }
 }));
 
 export const useStats = () => useStatsStore(state => state.stats);
+export const useStatsControls = () => useStatsStore(state => state.actions);
