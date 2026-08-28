c6b14c22c6f082d12d3ce83eec2ab31213b9fd9f
Andres Laverde <andres.laverde@protonmail.com>
2026-08-28 11:57:32 -0700

    update: implement initialize anecdotes when app renders

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import { useAnecdoteActions } from "./store";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 
 const App = () => {
+  const { initialize } = useAnecdoteActions();
+
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
+
   return (
     <div>
       <Filter />
--- anecdotes/src/store.js
+++ anecdotes/src/store.js
@@ -1,14 +1,6 @@
 
 import { create } from 'zustand';
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
+import anecdoteService from './services/anecdotes';
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -19,8 +11,8 @@ const asObject = anecdote => ({
 });
 
 const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
-  filterString : '',
+  anecdotes: [],
+  filterString: '',
   actions: {
     vote: (id) => set(state => (
       {
@@ -38,9 +30,13 @@ const useAnecdoteStore = create((set) => ({
         anecdotes: [...state.anecdotes, asObject(anec)]
       }
     )),
-    setFilter: (str) => set(() => ({ 
+    setFilter: (str) => set(() => ({
       filterString: str
-     }))
+    })),
+    initialize: async () => {
+      const anecdotes = await anecdoteService.getAll();
+      set(() => ({ anecdotes: anecdotes }));
+    }
   },
 }));
 
