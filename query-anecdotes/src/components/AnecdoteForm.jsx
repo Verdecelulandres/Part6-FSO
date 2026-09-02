55557e68161dc081e73fe73c8501a5ee0d9519a1
Andres Laverde <andres.laverde@protonmail.com>
2026-09-01 18:50:29 -0700

    update: show notification on anecdote creation

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,12 +1,17 @@
 import { useAnecdotes } from "../hooks/useAnecdotes";
+import { useContext } from "react";
+import NotificationContext from "../NotificationContext";
+
 
 const AnecdoteForm = () => {
+  const { displayMessage } = useContext(NotificationContext);
   const { addAnecdote } = useAnecdotes();
 
   const onCreate = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     addAnecdote(content);
+    displayMessage(`anecdote '${content}' created`);
     event.target.reset()
   }
 
